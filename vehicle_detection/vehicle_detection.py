0542eb4e354b8f6c1355b4b5b828294fdcb0a0c1
vinayakkgarg <vinayakk.garg99@gmail.com>
2018-07-15 21:59:15 +0530

    adding all files

---
+++ vehicle_detection/vehicle_detection.py
@@ -0,0 +1,29 @@
+#car Detetction
+
+import cv2
+
+
+video_capture = cv2.VideoCapture('dataset/video1.avi')
+#video_capture = cv2.VideoCapture('dataset/video2.avi')
+#video_capture = cv2.VideoCapture('dataset/video3.mp4')
+#video_capture = cv2.VideoCapture('dataset/video4.mp4')
+
+car_cascade = cv2.CascadeClassifier('cars.xml')
+
+while True:
+    ret, img = video_capture.read()
+    if (type(img) == type(None)):
+        break
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    cars = car_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x,y,w,h) in cars:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)      
+    
+    cv2.imshow('car_detected', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
---
+++ webapp/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/vinayakk/Desktop/Flask Apps/challan_project/license_plates.db'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos = todos)
+
+
+@app.route('/add', methods = ['POST'])
+def add():
+    todo = Todo(text = request.form['todoitem'], complete=False)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update', methods = ['POST'])
+def update():
+    print(request.form)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
